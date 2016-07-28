7c68f5e23ff5c9e711bd259dd633cd267f182270
YourtionGuo <yourtion@gmail.com>
2016-07-28 11:37:09 +0800

    init Xcode Project

---
+++ XcodeProject/LearningOpenCV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearningOpenCV
+//
+//  Created by YourtionGuo on 7/28/16.
+//  Copyright © 2016 Yourtion. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
